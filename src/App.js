5434b5910dcd3e27f1d6fb10fb4bec553737d109
jemai-sameh <samehjemai98@mail.com>
2023-07-07 18:13:37 +0100

    add search action

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 //import {searchIcon} from './search.svg';
 import MovieCard from './MovieCard';
-
+import searchIcon from './search.svg';
 const API_URL = "http://www.omdbapi.com?apikey=82ce53d3";
 
 const movie1 = {
@@ -16,6 +16,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm,setSearchTerm]=useState()
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -31,8 +32,8 @@ const App = () => {
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input placeholder="Search for movies" value="Superman" onChange={() => { }} />
-        <img src="./search.svg" alt="search" onClick={() => { }} />
+        <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={searchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
       {
         movies?.length > 0 ? (
