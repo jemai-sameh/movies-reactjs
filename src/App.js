99e5551938b21c4006f29924242554d5c80ec4c5
jemai-sameh <samehjemai98@mail.com>
2023-07-07 17:58:08 +0100

    add list of movies using api omdba

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
 import { React} from "react";
-import {useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 //import {searchIcon} from './search.svg';
+import MovieCard from './MovieCard';
 
+const API_URL = "http://www.omdbapi.com?apikey=82ce53d3";
 
-const API_URL="http://www.omdbapi.com?apikey=82ce53d3";
-
-const movie1={
-  "Title":"Amazin Spriderman ",
-  "Year": "2015",
-  "imdbID":"tt2586634",
-  "Type":"movie",
-  "Poster":"N/A"
+const movie1 = {
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+  "Title": "Italian Spiderman",
+  "Type": "movie",
+  "Year": "2007",
+  "imdbID": "tt2705436"
 }
 
-const App=()=>{
-      const searchMovies= async(title)=>{
-        const response=await fetch(`${API_URL}&s=${title}`);
-        const data=await response.json();
-        console.log(data);      
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []
+  )
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input placeholder="Search for movies" value="Superman" onChange={() => { }} />
+        <img src="./search.svg" alt="search" onClick={() => { }} />
+      </div>
+      {
+        movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+
+        ) : (
+          <div className="emplty">
+            <h2>No movies found</h2>
+          </div>
+        )
       }
 
-      useEffect(()=>{
-        searchMovies('Spiderman');
-      },[]
-      )
-    return (
-      <div className="app">
-        <h1>MovieLand</h1>
-        <div className="search">
-          <input placeholder="Search for movies" value="Superman" onChange={()=>{}}/>
-          <img src="./search.svg" alt="search" onClick={()=>{}} />
-        </div>
-          <div className="container">
-            <div className="movie">
-            <div>
-                <p>{movie1.Year}</p>
-            </div>
-            <div>
-              <img src={movie1.Poster!=='N/A'? movie1.Poster:'https://via.placeholder.com/400'} alt={movie1.Title}/>
-            </div>
-            <div>
-              <span>{movie1.Type}</span>
-              <h3>{movie1.Title}</h3>
-            </div>
-            </div>
-            
-          </div>
-         
-        </div>
-      );
+    </div>
+  );
 
 }
 
